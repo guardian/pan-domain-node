aee0b267605f91d3328d3ba8da695ffb72f3f2c2
Chris Finch <chris.finch@guardian.co.uk>
2015-07-28 11:21:30 +0100

    readme, aws settings, tests

--- README.md
+++ README.md
@@ -4,3 +4,49 @@ NodeJs implementation of pan-domain auth verification
 ## Installation
 
 `npm install --save git://github.com/guardian/pan-domain-node.git`
+
+
+## Usage
+
+Setup:
+
+```
+// app.js
+
+var PanDomainNode = require('pan-domain-node');
+
+PanDomainNode.setLogLevel('info');
+
+PanDomainNode.setAwsBucketDetails('MY_AWS_BUCKET', 'KEY_TO_MY_AWS_PUBLIC_KEY');
+
+```
+
+As express.js / connect style middleware:
+
+```
+// index.js
+
+var pandaAuthMiddleware = require('pan-domain-node').Middleware;
+
+router.get('/', pandaAuthMiddleware, function (req, res, next) {
+    var guardianUser = req.guUser;
+});
+
+```
+
+Error handling (see (express.js docs)[http://expressjs.com/guide/error-handling.html]):
+
+```
+// app.js
+
+var PANDA_AUTH_ERROR_MESSAGE = PanDomainNode.PandaAuthFailedErrorMessage;
+
+app.use(function(err, req, res, next) {
+    if (err === PANDA_AUTH_ERROR_MESSAGE) {
+        // redirect to sign in
+        res.status(401).send('Please sign in to a gutools.co.uk application.');
+    } else {
+        next(err);
+    };
+});
+```
---
+++ spec/pandaAuthSpec.js
@@ -0,0 +1,42 @@
+var Qs = require('qs');
+
+var PandaUtils = require('./../lib/pandaUtils');
+
+var cookieFixture = require('./fixtures/sampleCookie').cookie;
+var samplePublicKey = require('./fixtures/samplePublicKey').samplePublicKey;
+
+describe('jasmine-node', function(){
+
+    var parsedCookie;
+
+    it('should parse a cookie', function () {
+        parsedCookie = PandaUtils.parseCookie(cookieFixture);
+
+        expect(parsedCookie.hasOwnProperty('data')).toBe(true);
+        expect(parsedCookie.data.length).toEqual(320);
+        expect(parsedCookie.hasOwnProperty('signature')).toBe(true);
+        expect(parsedCookie.signature.length).toEqual(684);
+    });
+
+    it('should base64 decode a cookie data', function () {
+        var decodedData = PandaUtils.decodeBase64(parsedCookie.data);
+
+        expect(decodedData.length).toEqual(239);
+
+        var parsedDecodedData = Qs.parse(decodedData);
+
+        expect(parsedDecodedData.firstName).toBe('Chris');
+        expect(parsedDecodedData.lastName).toBe('Finch');
+    });
+
+    it('should correctly format a PEM key', function () {
+        var formattedKey = PandaUtils.stringToRSAPublicFormat(samplePublicKey);
+        var splitKey = formattedKey.split('\n');
+
+        expect(splitKey.length).toEqual(20);
+        expect(splitKey[0]).toBe('-----BEGIN PUBLIC KEY-----');
+        expect(splitKey[splitKey.length - 1]).toBe('-----END PUBLIC KEY-----');
+        expect(splitKey[9].length).toEqual(64);
+    });
+
+});
