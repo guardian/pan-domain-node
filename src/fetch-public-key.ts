7c6e204c17b205b68d610420a6ad7fd0594d139d
Pete F <pete.faulconbridge@theguardian.com>
2026-04-24 14:12:51 +0100

    Extract ini parsing function and add test to document functionality

--- src/fetch-public-key.ts
+++ src/fetch-public-key.ts
@@ -1,43 +1,50 @@
-import * as iniparser from 'iniparser';
-import {base64ToPEM, httpGet} from './utils';
 import { S3 } from "@aws-sdk/client-s3";
+import * as iniparser from "iniparser";
+import { base64ToPEM } from "./utils";
 
 export interface PublicKeyHolder {
-    key: string,
-    lastUpdated: Date
+  key: string;
+  lastUpdated: Date;
 }
 
+export function fetchPublicKey(
+  s3: S3,
+  bucket: string,
+  keyFile: string,
+): Promise<PublicKeyHolder> {
+  const publicKeyLocation = {
+    Bucket: bucket,
+    Key: keyFile,
+  };
 
-export function fetchPublicKey(s3: S3, bucket: string, keyFile: string): Promise<PublicKeyHolder> {
+  return s3
+    .getObject(publicKeyLocation)
+    .then(({ Body }) => Body?.transformToString())
+    .then((maybePandaConfigIni) => {
+      if (!maybePandaConfigIni) {
+        throw Error(
+          `could not read panda config ${JSON.stringify(publicKeyLocation)}`,
+        );
+      }
+      return parseKeysFromIni(maybePandaConfigIni);
+    })
+    .catch((error) => {
+      console.error(`Error fetching public key from S3: ${error}`);
+      throw error;
+    });
+}
 
-    const publicKeyLocation = {
-        Bucket: bucket,
-        Key: keyFile,
+export function parseKeysFromIni(pandaConfigIni: string): PublicKeyHolder {
+  const config: { publicKey?: string } = iniparser.parseString(pandaConfigIni);
+  if (config.publicKey) {
+    return {
+      key: base64ToPEM(config.publicKey, "PUBLIC"),
+      lastUpdated: new Date(),
     };
-
-    return s3.getObject(publicKeyLocation)
-        .then(({ Body }) => Body?.transformToString())
-        .then((pandaConfigIni) => {
-            if (!pandaConfigIni) {
-                throw Error(`could not read panda config ${JSON.stringify(publicKeyLocation)}`);
-            }
-            else {
-                const config: { publicKey?: string } = iniparser.parseString(pandaConfigIni);
-                if (config.publicKey) {
-                    return {
-                        key: base64ToPEM(config.publicKey, "PUBLIC"),
-                        lastUpdated: new Date()
-                    };
-                } else {
-                    console.log(`Failed to retrieve panda public key from ${JSON.stringify(config)}`);
-                    throw new Error("Missing publicKey setting from config");
-                }
-            }
-        })
-        .catch((error) => {
-            console.error(`Error fetching public key from S3: ${error}`);
-            throw error;
-        });
+  } else {
+    console.log(
+      `Failed to retrieve panda public key from ${JSON.stringify(config)}`,
+    );
+    throw new Error("Missing publicKey setting from config");
+  }
 }
-
-
